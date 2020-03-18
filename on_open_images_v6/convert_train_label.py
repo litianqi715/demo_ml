adbcc43c3b5b5d16194711a5d6ff8fc86e606c33
Tianqi Li <litianqi123@gmail.com>
2020-03-18 21:04:06 +0800

    add train label convert

--- on_open_images_v6/convert_train_label.py
+++ on_open_images_v6/convert_train_label.py
@@ -8,8 +8,9 @@ label_map_path = "D:\\open_images\\4metadata\\oidv6-class-descriptions.csv"
 train_label_path = "D:\\open_images\\1human\\oidv6-train-annotations-human-imagelabels.csv"
 json_file = "D:\\open_images\\1human\\oidv6-train-annotations-human-imagelabels.json"
 
+json_hierarchy_file = "D:\\open_images\\bbox_labels_600_hierarchy.json"
 
- # get label_id_to_name dict
+# get label_id_to_name dict
 df = pd.read_csv(label_map_path)
 label_ids = np.array(df['LabelName'])
 label_names = np.array(df['DisplayName'])
@@ -18,27 +19,50 @@ for idx, (label_id, label_name) in enumerate(zip(label_ids, label_names)):
     label_id_to_name[label_id] = label_name
 print("got label map dict")
 
-# get train_imgs_label dict
-start_t = time.time()
-df = pd.read_csv(train_label_path)
-image_ids = np.array(df['ImageID'])
-label_names = np.array(df['LabelName'])
-confidences = np.array(df['Confidence']) > 0
-valid_image_ids = image_ids[confidences]
-valid_label_ids = label_names[confidences]
 
-train_imgs_label = defaultdict(list)
-for idx, (image_id, label_id) in enumerate(zip(valid_image_ids, valid_label_ids)):
-    train_imgs_label[image_id].append( label_id_to_name[label_id] )
-del image_ids, label_names, confidences, valid_image_ids, valid_label_ids
-print("got img label list, use time {}".format(time.time() - start_t))
-json.dump(train_imgs_label, open(json_file, "w"))
-del train_imgs_label
-print("saved")
+def convert_json_hierarchy(hierarchy):
+    if isinstance(hierarchy, dict):
+        for key, val in hierarchy.items():
+            if key == "LabelName":
+                if val in label_id_to_name:
+                    label_name = label_id_to_name[val]
+                    hierarchy[key] = label_name
+            elif isinstance(val, list):
+                    hierarchy[key] = convert_json_hierarchy(val)
+    elif isinstance(hierarchy, list):
+        for idx, elem in enumerate(hierarchy):
+            hierarchy[idx] = convert_json_hierarchy(elem)
+    return hierarchy
 
-start_t = time.time()
-train_imgs_label = json.load(open(json_file))
-print("reload img label list, use time {}".format(time.time() - start_t))
+json_hierarchy = json.load(open(json_hierarchy_file))
+json_hierarchy_name = convert_json_hierarchy(json_hierarchy)
+json.dump(json_hierarchy_name, open("D:\\open_images\\bbox_labels_600_hierarchy.name.json", "w"))
+
+print("done")
+
+
+def convert_train_label():
+    # get train_imgs_label dict
+    start_t = time.time()
+    df = pd.read_csv(train_label_path)
+    image_ids = np.array(df['ImageID'])
+    label_names = np.array(df['LabelName'])
+    confidences = np.array(df['Confidence']) > 0
+    valid_image_ids = image_ids[confidences]
+    valid_label_ids = label_names[confidences]
+
+    train_imgs_label = defaultdict(list)
+    for idx, (image_id, label_id) in enumerate(zip(valid_image_ids, valid_label_ids)):
+        train_imgs_label[image_id].append( label_id_to_name[label_id] )
+    del image_ids, label_names, confidences, valid_image_ids, valid_label_ids
+    print("got img label list, use time {}".format(time.time() - start_t))
+    json.dump(train_imgs_label, open(json_file, "w"))
+    del train_imgs_label
+    print("saved")
+
+    start_t = time.time()
+    train_imgs_label = json.load(open(json_file))
+    print("reload img label list, use time {}".format(time.time() - start_t))
 
 
 # stat label dist
