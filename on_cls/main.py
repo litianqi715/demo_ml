4634210f245ef43299bc5c7e615d724b3310dc93
Tianqi Li <litianqi123@gmail.com>
2020-04-02 01:43:01 +0800

    good to run resnet34 and densenet121

--- on_cls/main.py
+++ on_cls/main.py
@@ -6,26 +6,31 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-
 import torchvision
 import torchvision.transforms as transforms
-
 import os
 import argparse
 from time import time
+import logging.config
 
 from models import *
 # from utils import progress_bar
-progress_bar = print
-
+# progress_bar = print
+log_config = "logging.conf"
+log_config = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logging.conf')
+os.chdir("on_cls")
+logging.config.fileConfig(log_config)
+logger = logging.getLogger('mind')
+os.chdir("..")
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
+data_base_dir = "D:/cifar-10/"
+ckpt_base_dir = "D:/cifar-10/"
 
 
 # Training
 def train(epoch):
-    print('\nEpoch: %d' % epoch)
+    logger.info('==>  Epoch: %d' % epoch)
     epoch_start = time()
     net.train()
     train_loss = 0
@@ -46,9 +51,10 @@ def train(epoch):
         correct += predicted.eq(targets).sum().item()
 
         use_time = time() - start_time
-        progress_bar(epoch, batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total), "in %fs"% use_time )
-    print("\nEpoch time: %f" % (time()-epoch_start))
+        outinfo = "%d %d %d Loss: %.3f | Acc: %.3f%% (%d/%d) in %fs" % (epoch, batch_idx, 
+          len(trainloader),train_loss/(batch_idx+1), 100.*correct/total, correct, total, use_time)
+        logger.info(outinfo)
+    logger.info("==>  Epoch %d time: %f" % (epoch, time()-epoch_start))
 
 # test
 def test(epoch):
@@ -68,21 +74,22 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            logger.info("%d %d Loss: %.3f | Acc: %.3f%% (%d/%d)" % 
+                (batch_idx, len(testloader), test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
     # Save checkpoint.
     acc = 100.*correct/total
     if acc > best_acc:
-        print('Saving..')
+        print('==> Saving..')
         state = {
             'net': net.state_dict(),
             'acc': acc,
             'epoch': epoch,
         }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
-        torch.save(state, './checkpoint/ckpt_epoch{}.t7'.format(epoch))
+        ckpt_folder = ckpt_base_dir + 'checkpoint_{}/'.format(model_name)
+        if not os.path.isdir(ckpt_folder):
+            os.mkdir(ckpt_folder)
+        torch.save(state, ckpt_folder+'ckpt_epoch{}.pth'.format(epoch))
         best_acc = acc
 
 
@@ -104,7 +111,7 @@ if __name__=="__main__":
     start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
     # Data
-    print('==> Preparing data..')
+    logger.info('==> Preparing data..')
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
@@ -117,27 +124,29 @@ if __name__=="__main__":
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
-    base_batch_size = 224
+    base_batch_size = 48
     batch_size = base_batch_size * torch.cuda.device_count()
-    trainset = torchvision.datasets.CIFAR10(root='D:\\cifar-10\\', train=True, download=True, transform=transform_train)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+    trainset = torchvision.datasets.CIFAR10(root=data_base_dir, train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=3)
 
-    testset = torchvision.datasets.CIFAR10(root='D:\\cifar-10\\', train=False, download=True, transform=transform_test)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=4)
+    testset = torchvision.datasets.CIFAR10(root=data_base_dir, train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=base_batch_size, shuffle=False, num_workers=1)
 
     # Model
-    print('==> Building model..')
-    net = VGG('VGG19')
+    logger.info('==> Building model..')
+    # net = VGG('VGG19')
     # net = ResNet18()
+    # net = ResNet34()
     # net = PreActResNet18()
     # net = GoogLeNet()
-    # net = DenseNet121()
+    net = DenseNet121()
     # net = ResNeXt29_2x64d()
     # net = MobileNet()
     # net = MobileNetV2()
     # net = DPN92()
     # net = ShuffleNetG2()
     # net = SENet18()
+    model_name = "densenet121"
     net = net.to(device)
     if device == 'cuda':
         net = torch.nn.DataParallel(net)
@@ -145,7 +154,7 @@ if __name__=="__main__":
 
     if args.resume:
         # Load checkpoint.
-        print('==> Resuming from checkpoint..')
+        logger.info('==> Resuming from checkpoint..')
         assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
         checkpoint = torch.load('./checkpoint/ckpt.t7')
         net.load_state_dict(checkpoint['net'])
@@ -155,6 +164,7 @@ if __name__=="__main__":
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 
+    logger.info("==> ")
     for epoch in range(start_epoch, start_epoch+200):
         train(epoch)
         test(epoch)
